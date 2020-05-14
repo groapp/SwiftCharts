4dbe31798c9a50922eb5d8a02e02009c568b4dbe
Austin Duke <austin.r.duke@gmail.com>
2020-05-14 17:26:12 -0500

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -11,6 +12,14 @@ let package = Package(
         .library(
             name: "SwiftCharts",
             targets: ["SwiftCharts"]),
+    ],
+    dependencies: [
+    // Here we define our package's external dependencies
+    // and from where they can be fetched:
+        .package(
+            url: "https://github.com/i-schuetz/SwiftCharts.git",
+            from: "0.6.5"
+        ),
     ],
     targets: [
         .target(
